3a36c482d8d1c778c9c98db024313b5bb3272002
Vihar Harshukhalal <viharharshukhalal.2003@gmail.com>
2023-11-12 16:19:33 +0000

    added images

--- lessons.js
+++ lessons.js
@@ -3,12 +3,12 @@ const lessons = [
     {id: 2, subject: 'Science', location: 'Lab A', price: 25, spaces: 5, image:"ScienceClass Logo.jpg"},
     {id: 3, subject: 'Art', location: 'Art Studio', price: 15, spaces: 5, image:"ArtClass Logo.jpg"},
     {id: 4,subject: 'English', location: 'Room 123', price: 10, spaces: 5, image:"EnglishClass Logo.jpg"},
-    {id: 5, subject: 'Geography', location: 'Room B16', price: 7.99, spaces: 5, image:""},
-    {id: 6, subject: 'Physics', location: 'Room C32', price: 20, spaces: 5, image:""},
-    {id: 7, subject: 'Biology', location: 'Lab B', price: 20, spaces: 5, image:""},
-    {id: 8, subject: 'Chemistry', location: 'Lab B2', price: 20, spaces: 5, image:""},
-    {id: 9, subject: 'History', location: 'Room 42', price: 15, spaces: 5, image:""},
-    {id: 10, subject: 'Computer Science', location: 'Lab C1', price: 25, spaces: 5, image:""},
+    {id: 5, subject: 'Geography', location: 'Room B16', price: 7.99, spaces: 5, image:"geographyClass.png"},
+    {id: 6, subject: 'Physics', location: 'Room C32', price: 20, spaces: 5, image:"physicsClass.png"},
+    {id: 7, subject: 'Biology', location: 'Lab B', price: 20, spaces: 5, image:"biology class.png"},
+    {id: 8, subject: 'Chemistry', location: 'Lab B2', price: 20, spaces: 5, image:"chemistryClass.png"},
+    {id: 9, subject: 'History', location: 'Room 42', price: 15, spaces: 5, image:"historyClass.png"},
+    {id: 10, subject: 'Computer Science', location: 'Lab C1', price: 25, spaces: 5, image:"computerClass.png"},
 ];
 
 export default lessons;
