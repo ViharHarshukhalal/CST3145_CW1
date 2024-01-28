a001b2f4166e3ad6f52eabcd854d247d7cbb8886
Vihar Harshukhalal <viharharshukhalal.2003@gmail.com>
2024-01-28 18:20:28 +0000

    moved images in one directory

--- src/app.vue
+++
@@ -1,9 +0,0 @@
-created() {
-    fetch('http://localhost:3000/api/lessons')  // Update this URL
-      .then(response => response.json())
-      .then(data => {
-        console.log('Lessons:', data);
-        // Update your Vue.js app state with the fetched data
-      })
-      .catch(error => console.error('Error fetching lessons:', error));
-  }
