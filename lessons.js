037a75786ed3194ef6729d67365bba6a05232466
Vihar Harshukhalal <viharharshukhalal.2003@gmail.com>
2024-02-15 16:44:08 +0000

    updates

--- lessons.js
+++ lessons.js
@@ -1,14 +1,14 @@
-const lessons = [
-    {id: 1, subject: 'Math', location: 'Room 101', price: 20, spaces: 5, image:"images/MathClass Logo.jpg"},
-    {id: 2, subject: 'Science', location: 'Lab A', price: 25, spaces: 5, image:"images/ScienceClass Logo.jpg"},
-    {id: 3, subject: 'Art', location: 'Art Studio', price: 15, spaces: 5, image:"images/ArtClass Logo.jpg"},
-    {id: 4,subject: 'English', location: 'Room 123', price: 10, spaces: 5, image:"images/EnglishClass Logo.jpg"},
-    {id: 5, subject: 'Geography', location: 'Room B16', price: 7.99, spaces: 5, image:"images/geographyClass.png"},
-    {id: 6, subject: 'Physics', location: 'Room C32', price: 20, spaces: 5, image:"images/physicsClass.png"},
-    {id: 7, subject: 'Biology', location: 'Lab B', price: 20, spaces: 5, image:"images/biology class.png"},
-    {id: 8, subject: 'Chemistry', location: 'Lab B2', price: 20, spaces: 5, image:"images/chemistryClass.png"},
-    {id: 9, subject: 'History', location: 'Room 42', price: 15, spaces: 5, image:"images/historyClass.png"},
-    {id: 10, subject: 'Computer Science', location: 'Lab C1', price: 25, spaces: 5, image:"images/computerClass.png"},
-];
+// const lessons = [
+//     {id: 1, subject: 'Math', location: 'Room 101', price: 20, spaces: 5, image:"images/MathClass Logo.jpg"},
+//     {id: 2, subject: 'Science', location: 'Lab A', price: 25, spaces: 5, image:"images/ScienceClass Logo.jpg"},
+//     {id: 3, subject: 'Art', location: 'Art Studio', price: 15, spaces: 5, image:"images/ArtClass Logo.jpg"},
+//     {id: 4,subject: 'English', location: 'Room 123', price: 10, spaces: 5, image:"images/EnglishClass Logo.jpg"},
+//     {id: 5, subject: 'Geography', location: 'Room B16', price: 7.99, spaces: 5, image:"images/geographyClass.png"},
+//     {id: 6, subject: 'Physics', location: 'Room C32', price: 20, spaces: 5, image:"images/physicsClass.png"},
+//     {id: 7, subject: 'Biology', location: 'Lab B', price: 20, spaces: 5, image:"images/biology class.png"},
+//     {id: 8, subject: 'Chemistry', location: 'Lab B2', price: 20, spaces: 5, image:"images/chemistryClass.png"},
+//     {id: 9, subject: 'History', location: 'Room 42', price: 15, spaces: 5, image:"images/historyClass.png"},
+//     {id: 10, subject: 'Computer Science', location: 'Lab C1', price: 25, spaces: 5, image:"images/computerClass.png"},
+// ];
 
-export default lessons;
+// export default lessons;
